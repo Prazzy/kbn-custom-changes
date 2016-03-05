d4b50e4a3dc0237a77e271e85b51e3fb12148d3f
Praveen <prazzee@gmail.com>
2016-03-05 12:09:36 -0800

    added src folder

---
+++ kibana-4.4.1/src/plugins/kibana/public/kibana.js
@@ -0,0 +1,57 @@
+require('plugins/kibana/discover/index');
+require('plugins/kibana/visualize/index');
+require('plugins/kibana/dashboard/index');
+require('plugins/kibana/settings/index');
+require('plugins/kibana/doc/index');
+require('ui/timepicker');
+
+var moment = require('moment-timezone');
+
+var chrome = require('ui/chrome');
+var routes = require('ui/routes');
+var modules = require('ui/modules');
+
+var kibanaLogoUrl = require('ui/images/kibana.svg');
+
+routes.enable();
+
+routes
+.otherwise({
+  //redirectTo: `/${chrome.getInjected('kbnDefaultAppId', 'discover')}`
+  redirectTo: `/Media-Usage`
+});
+
+chrome
+.setBrand({
+  'logo': 'url(' + kibanaLogoUrl + ') left no-repeat',
+  'smallLogo': 'url(' + kibanaLogoUrl + ') left no-repeat'
+})
+.setNavBackground('#222222')
+.setTabDefaults({
+  resetWhenActive: true,
+  lastUrlStore: window.sessionStore,
+  activeIndicatorColor: '#656a76'
+})
+.setTabs([
+  {
+    id: 'Media-Usage',
+    title: 'Media Usage'
+  },
+  {
+    id: 'Portal-Usage',
+    title: 'Portal Usage'
+  }
+])
+.setRootController('kibana', function ($scope, $rootScope, courier, config) {
+  function setDefaultTimezone() {
+    moment.tz.setDefault(config.get('dateFormat:tz'));
+  }
+
+  // wait for the application to finish loading
+  $scope.$on('application.load', function () {
+    courier.start();
+  });
+
+  $scope.$on('init:config', setDefaultTimezone);
+  $scope.$on('change:config.dateFormat:tz', setDefaultTimezone);
+});
